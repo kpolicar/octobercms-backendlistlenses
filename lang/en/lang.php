77b6ff9c1267e2b6d5e94f2b262c1e1195a58203
Klemen Poličar <klemen.janez.policar@gmail.com>
2021-07-17 15:48:09 +0200

    update plugin description

--- lang/en/lang.php
+++ lang/en/lang.php
@@ -3,7 +3,7 @@
 return [
     'plugin' => [
         'name' => 'Backend List Lenses',
-        'description' => '...',
+        'description' => 'Lenses allow you to create backend pages with customized queries to better represent your data.',
         'tab' => 'Customization',
     ],
     'list' => [
